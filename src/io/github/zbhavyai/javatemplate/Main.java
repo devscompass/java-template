8faed53acf6e4638097bafc41c08c25212ed7b5d
Bhavyai Gupta <zbhavyai@gmail.com>
2025-10-18 23:24:50 -0600

    add hello world code

---
+++ src/io/github/zbhavyai/javatemplate/Main.java
@@ -0,0 +1,7 @@
+package io.github.zbhavyai.javatemplate;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
